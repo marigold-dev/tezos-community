4322fd97d5346c1e12b53cdf003c311f0f954a69
Benjamin Fuentes <zamrokk@gmail.com>
2023-06-02 15:59:55 +0200

    fix session backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,19 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as session from 'express-session';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {});
   app.enableCors({ origin: '*', credentials: true });
-  /*app.use(cookieParser());
+  /*app.use(cookieParser());*/
   app.use(
     session({
       secret: 'YOUWILLNEVERGUESSWHATITIS',
       resave: true,
       saveUninitialized: false,
     }),
-  );*/
+  );
   await app.listen(3001);
   Logger.log(`Application is running on: ${await app.getUrl()}`);
 }
