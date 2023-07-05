1fe92a787821da89fd2f5b04ecff7fab3b0eb136
Benjamin Fuentes <zamrokk@gmail.com>
2023-07-05 16:36:58 +0200

    env

--- app/src/pages/FAQScreen.tsx
+++ app/src/pages/FAQScreen.tsx
@@ -259,6 +259,7 @@ export const FAQScreen: React.FC = () => {
               "_blank"
             )
           }
+          color="transparent"
           target="_blank"
         >
           Documentation
